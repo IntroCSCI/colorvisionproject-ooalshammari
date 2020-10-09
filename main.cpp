06a722281c40190a36b0ada188555abb80659560
Othman Alshammari <ooalshammari@mail.csuchico.edu>
2020-10-09 19:43:13 +0000

    i have declared variables to retrieve patient RGB report

--- main.cpp
+++ main.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main()
 {
-  
+  int count = 0;
+  // my project
+  cout<< "Enter Your to tatal number of color vision (RGB) tests"<<"\n";
+
+  cin >> count;
+  int R[count];
+   int G[count];
+   int B[count];
+cout<< "Enter RGB values of test in sequence 1st R 2nd G 3rd B for all test"<<"\n";
+
+
+   for (int i =0; i < count; i++)
+   {
+     cin>> R[i];
+      cin>> G[i];
+       cin>> B[i];
+   }
+
   return 0;
 }
